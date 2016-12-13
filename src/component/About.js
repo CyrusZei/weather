c70888abf7a1c791b1f05eab63d74faa53534340
Cyrus Zei <cyruszei@gmail.com>
2016-12-13 11:14:17 +0100

    the routing working just fine

---
+++ src/component/About.js
@@ -0,0 +1,11 @@
+import React, { Component} from 'react';
+
+var About = React.createClass({
+  render:function(){
+    return(
+      <div>About</div>
+    );
+  }
+});
+
+export default About;
--- src/index.js
+++ src/index.js
@@ -4,13 +4,14 @@ import {Route, Router, IndexRoute, hashHistory} from 'react-router';
 import App from './App';
 import './index.css';
 import Weather from './component/Weather';
-
+import About from './component/About';
 
 
 
 ReactDOM.render(
   <Router history={hashHistory}>
     <Route path='/' component={App}>
+      <Route path='about' component={About}/>
       <IndexRoute component={Weather}/>
     </Route>
   </Router>,
